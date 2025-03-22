9846f7819fff04c8d4dacf37af82d14cc0f04198
Adex324 <o.joeade@gmail.com>
2025-03-22 14:05:00 +0100

    final

--- weatherproject/weatherproject/urls.py
+++ weatherproject/weatherproject/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('weatherapp.urls')),
+    path('',include('weatherproject.weatherapp.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
